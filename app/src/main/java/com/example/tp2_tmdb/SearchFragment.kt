962b8a3813522348ce0f68ca2cd1d9dfdf9f2fa3
RomainPoutot <poutot.romain@gmail.com>
2022-01-10 09:15:45 +0100

    Set up navigation

---
+++ app/src/main/java/com/example/tp2_tmdb/SearchFragment.kt
@@ -0,0 +1,37 @@
+package com.example.tp2_tmdb
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
+
+class SearchFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_search, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val button = view.findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            ChangeActivity(view)
+        }
+    }
+
+    fun ChangeActivity(view: View) {
+        findNavController().navigate(R.id.action_searchFragment_to_itemFragment)
+    }
+}
